e03799f43c0114bf56b73e146c9b5b52c66ee42f
大猫猫 <xianglvs@gmail.com>
2017-05-23 22:03:38 +0800

    优化代码

--- public/js/manage/login.js
+++ public/js/manage/login.js
@@ -1,24 +1,26 @@
 /**
  * Created by Administrator on 2017/4/27.
  */
-$(function () {
-    // bg switcher
-    var $btns = $(".bg-switch .bg");
-    $btns.click(function (e) {
-        e.preventDefault();
-        $btns.removeClass("active");
-        $(this).addClass("active");
-        var bg = $(this).data("img");
+(function ($, window) {
+    $(function () {
+        // bg switcher
+        var $btns = $('.bg-switch .bg');
+        $btns.click(function (e) {
+            e.preventDefault();
+            $btns.removeClass('active');
+            $(this).addClass('active');
+            var bg = $(this).data('img');
 
-        $("html").css("background-image", "url('/plugin/theme/default/img/bgs/" + bg + "')");
-    });
+            $('html').css('background-image', "url('/plugin/theme/default/img/bgs/" + bg + "')");
+        });
 
-    $('#login').click(function(){
-        $.post('/manage/signin',$('form[name="login"]').serialize(),function(data){
-            console.log(data);
-            if(data.result){
-                window.location.href = '/manage/panel';
-            }
+        $('#login').click(function () {
+            $.post('/manage/signin', $('form[name="login"]').serialize(), function (data) {
+                console.log(data);
+                if (data.result) {
+                    window.location.href = '/manage/panel';
+                }
+            });
         });
     });
-});
+}(this.jQuery, this));
