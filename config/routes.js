f2efdeb280bd9979ba05d1c5b45fc4970d7d78d6
大猫咪 <xianglvs@gmail.com>
2017-05-25 17:57:43 +0800

    添加自动路由

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+const fs = require('fs'); // 文件读写工具包
+const controllersPath = 'app/controllers';
 /*
  * Module dependencies.
  */
@@ -22,35 +25,64 @@ const fail = {
 module.exports = function (app, passport) {
   const pauth = passport.authenticate.bind(passport);
 
-  //登录相关路由
+  // 登录相关路由
   require('../app/routes/manage/login')(app, passport);
 
-  //后台权限部分
+  // 后台权限部分
   app.use(function (req, res, next) {
-    if(req.session.user!=undefined){
+    if (req.session.user != undefined) {
       next();
-    }else{
+    } else {
       res.redirect('/manage/login');
     }
   });
-  //面板路由
+
+  /**
+   * 自动扫描action添加到路由
+   * @param {*string} action 
+   */
+  var scanControllers = function (action) {
+    action = action || '';
+    let path = controllersPath;
+    if (action) {
+      path = controllersPath + action;
+    }
+    fs.readdirSync(path)
+      .forEach(function (file) {
+        if (fs.statSync(path + '/' + file).isDirectory()) {
+          scanControllers(action + '/' + file);
+        } else {
+          let controller = require('../' + path + '/' + file);
+          for (let method in controller) {
+            if (controller.hasOwnProperty(method)) {
+              console.log('scanning controller : ' + action + '/' + file.split('.')[0] + '/' + method);
+              app.all(action + '/' + file.split('.')[0] + '/' + method, controller[method]);
+            }
+          }
+        }
+      });
+  };
+
+  scanControllers();
+
+  // 面板路由
   require('../app/routes/manage/panel')(app, passport);
-  //用户路由
+  // 用户路由
   require('../app/routes/manage/user')(app, passport);
-  //文件路由
+  // 文件路由
   require('../app/routes/manage/file')(app, passport);
-  //区域路由
+  // 区域路由
   require('../app/routes/manage/area')(app, passport);
-  //菜单路由
+  // 菜单路由
   require('../app/routes/manage/menu')(app, passport);
-  //机构路由
+  // 机构路由
   require('../app/routes/manage/office')(app, passport);
-  //字典路由
+  // 字典路由
   require('../app/routes/manage/dict')(app, passport);
-  //角色路由
+  // 角色路由
   require('../app/routes/manage/role')(app, passport);
 
-  /*app.post('/user/session',
+  /* app.post('/user/session',
     pauth('local', {
       failureRedirect: '/login',
       failureFlash: 'Invalid email or password.'
@@ -63,21 +95,25 @@ module.exports = function (app, passport) {
    */
   app.use(function (err, req, res, next) {
     // treat as 404
-    if (err.message
-      && (~err.message.indexOf('not found')
-      || (~err.message.indexOf('Cast to ObjectId failed')))) {
+    if (err.message &&
+      (~err.message.indexOf('not found') ||
+        (~err.message.indexOf('Cast to ObjectId failed')))) {
       return next();
     }
 
     console.error(err.stack);
 
     if (err.stack.includes('ValidationError')) {
-      res.status(422).render('422', { error: err.stack });
+      res.status(422).render('422', {
+        error: err.stack
+      });
       return;
     }
 
     // error page
-    res.status(500).render('500', { error: err.stack });
+    res.status(500).render('500', {
+      error: err.stack
+    });
   });
 
   // assume 404 since no middleware responded
