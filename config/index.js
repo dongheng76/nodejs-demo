4bbf85a9c6c1ca89b83c03f70498b1d4bcf9e397
大猫咪 <xianglvs@gmail.com>
2017-05-25 11:16:44 +0800

    取消.env读取

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@
  */
 const path = require('path');                                 // 读取系统路径模块
 const extend = require('util')._extend;                       // 继承并扩展json对象方法
-const dotenv = require('dotenv').config();                    // 载入运行时配置文件
+// const dotenv = require('dotenv').config();                    // 载入运行时配置文件
 const development = require('./env/development');             // 开发模式环境
 const test = require('./env/test');                           // 测试模式环境
 const production = require('./env/production');               // 生产模式环境
@@ -22,14 +22,6 @@ const defaults = {                                              // 默认对象
   root: path.join(__dirname, '..'),                             // 项目根目录地址
   notifier: notifier
 };
-
-
-/**
- * ovoverride process.env
- */
-for (let k in dotenv) {
-  process.env[k] = dotenv[k];
-}
 
 /**
  * Expose
